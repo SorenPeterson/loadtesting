34eb9f059d89616b85d63b33ee9d7ebdb5abb8e6
Soren Peterson <soren468@gmail.com>
2016-03-04 15:42:46 -0800

    added a lot of tasks

--- locustio/main.py
+++ locustio/main.py
@@ -1,11 +1,37 @@
+import json
 from locust import HttpLocust, TaskSet, task
 
 class MapiTasks(TaskSet):
+    config = {
+        'auth': ('admin_consumer', 'marqeta')
+    }
+    
     def on_start(self):
-        pass
+        print self.config
+        response = self.client.post("/v3/users", auth = ('admin_consumer', 'marqeta'))
+        data = json.loads(response.content)
+        print data
+        self.token = data['token']
         
-    def index(self):
-        self.client.get("/")
+    @task
+    def ping(self):
+        self.client.get("/v3/ping")
+        
+    @task
+    def users(self):
+        self.client.get("/v3/users/%(self.token)" % locals(), auth = ('admin_consumer', 'marqeta'))
+        
+    @task
+    def authorization(self):
+        body = {
+            'amount': 100,
+            'mid': '12341234',
+            'card_token': 'doge',
+        }
+        self.client.post('/v3/simulate/authorization', auth = ('admin_consumer', 'marqeta'))
         
 class MapiUser(HttpLocust):
-    task_set = WebsiteTasks
+    host = "http://local.marqeta.com:8080"
+    task_set = MapiTasks
+    min_wait = 5000
+    max_wait = 15000
