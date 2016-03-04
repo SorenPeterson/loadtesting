1df8eefb7d4cca0454b53c6051e4ecf6bb5a72c9
Soren Peterson <soren468@gmail.com>
2016-03-04 14:36:23 -0800

    bring back the on start

--- locustio/main.py
+++ locustio/main.py
@@ -1,21 +1,11 @@
 from locust import HttpLocust, TaskSet, task
 
-class WebsiteTasks(TaskSet):
+class MapiTasks(TaskSet):
     def on_start(self):
-        self.client.post("/login", {
-            "username": "test_user",
-            "password": ""
-        })
-    
-    @task
+        pass
+        
     def index(self):
         self.client.get("/")
         
-    @task
-    def about(self):
-        self.client.get("/about/")
-
-class WebsiteUser(HttpLocust):
+class MapiUser(HttpLocust):
     task_set = WebsiteTasks
-    min_wait = 5000
-    max_wait = 15000
