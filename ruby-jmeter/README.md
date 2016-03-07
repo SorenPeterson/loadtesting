d5f5f1e27ca4dad85e69b897ab6f56446ebdb0c8
Soren Peterson <soren468@gmail.com>
2016-03-07 10:29:43 -0800

    test the ping route in ruby jmeter

--- ruby-jmeter/README.md
+++ ruby-jmeter/README.md
@@ -4,3 +4,4 @@ gem install ruby-jmeter
 
 Pros:
 * Written in Ruby so existing programming skills are leveraged. Less mental effort spent doing new things
+* You can make assertions on the data
--- ruby-jmeter/main.rb
+++ ruby-jmeter/main.rb
@@ -3,7 +3,7 @@ require 'ruby-jmeter'
 test do  
   with_json
   threads 1, loops: 5 do
-    get name: 'get_products_index',
-        url: 'http://example-rest-api.herokuapp.com/api/products'
+    get name: 'ping',
+        url:   'http://local.marqeta.com:8080/v3/ping'
   end
 end.run
