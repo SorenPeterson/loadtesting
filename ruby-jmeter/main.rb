8584b54ba287e9b2ff5fe8801f6b63a5ee74ad7f
Soren Peterson <soren468@gmail.com>
2016-03-07 11:22:35 -0800

    add gemfile

--- ruby-jmeter/main.rb
+++ ruby-jmeter/main.rb
@@ -1,9 +1,13 @@
 require 'ruby-jmeter'
 
+def url(route)
+  'https://local.marqeta.com:8080/v3' + route
+end
+
 test do  
   with_json
   threads 1, loops: 5 do
-    get name: 'ping',
-        url:   'http://local.marqeta.com:8080/v3/ping'
+    get name: 'Users Index',
+        url: url('/users')
   end
-end.run
+end.run(path: '/usr/local/bin/', gui: true)
