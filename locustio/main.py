bd45e67f8273a54eb80ad075f7ce5c26821aac19
Soren Peterson <soren468@gmail.com>
2016-03-07 15:51:17 -0800

    create the card and attempt an authorization

--- locustio/main.py
+++ locustio/main.py
@@ -3,24 +3,21 @@ from locust import HttpLocust, TaskSet, task
 
 class MapiTasks(TaskSet):
     def on_start(self):
-        response = self.client.post("/v3/users", json={}, auth=('admin_consumer', 'marqeta'), name="Create User")
+        response = self.client.post("/users", json={}, auth=('admin_consumer', 'marqeta'), name="Create User")
         data = json.loads(response.content)
-        self.token = data['token']
+        self.user_token = data['token']
+        
+        response = self.client.post("/cards", json={user_token: self.user_token, card_production_token: '86a4e6cf-6102-4895-bdab-d6fe0b18073b'}, auth=('admin_consumer', 'marqeta'))
+        data = json.loads(response.content)
+        self.card_token = data['token']
+        
         
     @task
-    def ping(self):
-        self.client.get("/v3/ping", name="Ping")
-        
-    @task
-    def users(self):
-        self.client.get("/v3/users", auth=('admin_consumer', 'marqeta'), name="Users Index")
-        
-    @task
-    def more_users(self):
-        self.client.get("/v3/users?count=100", auth=('admin_consumer', 'marqeta'), name="Users Index count=100")
+    def authorization(self):
+        self.client.post("/v3/simulate_authorization", json={card_token: self.card_token, mid: '123491239158', amount: 5.0} name="Authorization")
         
 class MapiUser(HttpLocust):
-    host = "http://local.marqeta.com:8080"
+    host = "http://local.marqeta.com:8080/v3"
     task_set = MapiTasks
     min_wait = 5000
     max_wait = 15000
