c07093f3365ff8100a36f00e0e857a490aea19f7
Soren Peterson <soren468@gmail.com>
2016-03-07 14:04:22 -0800

    name all test in locust

--- locustio/main.py
+++ locustio/main.py
@@ -3,21 +3,21 @@ from locust import HttpLocust, TaskSet, task
 
 class MapiTasks(TaskSet):
     def on_start(self):
-        response = self.client.post("/v3/users", json={}, auth=('admin_consumer', 'marqeta'))
+        response = self.client.post("/v3/users", json={}, auth=('admin_consumer', 'marqeta'), name="Create User")
         data = json.loads(response.content)
         self.token = data['token']
         
     @task
     def ping(self):
-        self.client.get("/v3/ping")
+        self.client.get("/v3/ping", name="Ping")
         
     @task
     def users(self):
-        self.client.get("/v3/users", auth=('admin_consumer', 'marqeta'))
+        self.client.get("/v3/users", auth=('admin_consumer', 'marqeta'), name="Users Index")
         
     @task
     def more_users(self):
-        self.client.get("/v3/users?count=100", auth=('admin_consumer', 'marqeta'))
+        self.client.get("/v3/users?count=100", auth=('admin_consumer', 'marqeta'), name="Users Index count=100")
         
 class MapiUser(HttpLocust):
     host = "http://local.marqeta.com:8080"
---
+++ locustio/test.py
@@ -0,0 +1,4 @@
+def create(self):
+    print self
+    
+create()
