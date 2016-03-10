ae1c6cc503b1426bbcdffb870a67620a98ef4060
Soren Peterson <soren468@gmail.com>
2016-03-10 11:07:31 -0800

    include gatling

---
+++ gatling/gatling-charts-highcharts-bundle-2.1.7/results/basicusercreation-1457570429070/js/stats.js
@@ -0,0 +1,203 @@
+var stats = {
+    type: "GROUP",
+name: "Global Information",
+path: "",
+pathFormatted: "group_missing-name-b06d1",
+stats: {
+    "name": "Global Information",
+    "numberOfRequests": {
+        "total": "2400",
+        "ok": "2400",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "17",
+        "ok": "17",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "1154",
+        "ok": "1154",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "35",
+        "ok": "35",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "78",
+        "ok": "78",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "21",
+        "ok": "21",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "28",
+        "ok": "28",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "62",
+        "ok": "62",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "443",
+        "ok": "443",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 2391,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 9,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "20.001",
+        "ok": "20.001",
+        "ko": "-"
+    }
+},
+contents: {
+"req_post-users-7e7ff": {
+        type: "REQUEST",
+        name: "POST Users",
+path: "POST Users",
+pathFormatted: "req_post-users-7e7ff",
+stats: {
+    "name": "POST Users",
+    "numberOfRequests": {
+        "total": "2400",
+        "ok": "2400",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "17",
+        "ok": "17",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "1154",
+        "ok": "1154",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "35",
+        "ok": "35",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "78",
+        "ok": "78",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "21",
+        "ok": "21",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "28",
+        "ok": "28",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "62",
+        "ok": "62",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "443",
+        "ok": "443",
+        "ko": "-"
+    },
+    "group1": {
+        "name": "t < 800 ms",
+        "count": 2391,
+        "percentage": 100
+    },
+    "group2": {
+        "name": "800 ms < t < 1200 ms",
+        "count": 9,
+        "percentage": 0
+    },
+    "group3": {
+        "name": "t > 1200 ms",
+        "count": 0,
+        "percentage": 0
+    },
+    "group4": {
+        "name": "failed",
+        "count": 0,
+        "percentage": 0
+    },
+    "meanNumberOfRequestsPerSecond": {
+        "total": "20.001",
+        "ok": "20.001",
+        "ko": "-"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
---
+++ gatling/gatling-charts-highcharts-bundle-2.1.7/user-files/simulations/main.scala
@@ -0,0 +1,57 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicUserCreation extends Simulation {
+
+  // Gatling Docs
+  // HTTP Requests: http://gatling.io/docs/2.0.0-RC2/http/http_request.html
+  // HTTP Checks (asserts): http://gatling.io/docs/2.0.0-RC2/http/http_check.html#http-check
+
+  /*
+  // All the headers in one
+
+  val all_headers = Map("Content-Type" -> "application/json",
+                        "Accept" -> "application/json", 
+                        "Authorization" -> "Basic YWRtaW5fY29uc3VtZXI6bWFycWV0YQ==")
+
+  // Just specific headers for adding individually
+
+  val content_type = ("Content-Type", "application/json")
+  val credz = ("Authorization", "Basic YWRtaW5fY29uc3VtZXI6bWFycWV0YQ==")
+  */
+
+  /* 
+  // you can also have JSON files elsewhere that you import rather than building inline
+  ./user-files/request-bodies/myFileBody.json
+  { "myContent": "${myValue}" }
+  .body(ELFileBody("myFileBody.json")).asJSON
+  */
+
+  val httpConf = http
+    .baseURL("http://local.marqeta.com:8080/v3") // Here is the root for all relative URLs
+    .acceptHeader("text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+
+  object CreateUser {
+    val create_user = exec(http("POST Users")
+      // the method and first class endpoint
+      .post("/users")
+      .basicAuth("admin_consumer", "marqeta") // helper! not necessary, but alternative to credz
+      .body(StringBody("""{}"""))
+      .asJSON // helper! not necessary, but defines request body format and dictates required response format
+      .check(status.is(201))
+    )
+  }
+
+  val users = scenario("Users").exec(CreateUser.create_user)
+
+  setUp(
+    users.inject(
+      constantUsersPerSec(20) during(120 seconds)
+    ).protocols(httpConf)
+  )
+}
