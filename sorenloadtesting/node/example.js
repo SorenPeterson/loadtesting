7799c5fdeab43745177d5f4fcdd93974e6babfa2
Soren Peterson <soren468@gmail.com>
2016-03-15 13:05:55 -0700

    add content-type to site definition to fix 415 errors in example

--- sorenloadtesting/node/example.js
+++ sorenloadtesting/node/example.js
@@ -1,19 +1,19 @@
 var Site = require('./sorensloadtestingtool.js');
 
-var site = new Site('http://local.marqeta.com:8080/v3').auth('admin_consumer', 'marqeta')
+var site = new Site('http://local.marqeta.com:8080/v3').auth('admin_consumer', 'marqeta').header('Content-Type', 'application/json');
 
 var createUserAndGet = site.createTaskFlow('Create and Get User');
 
 createUserAndGet.registerTask('Create User', function (goto, site, state) {
   site.post('POST Users', '/users', {/*empty body*/}, function (response, body) {
-    console.log(response.statusCode);
     state.user_token = JSON.parse(body)['token'];
+    console.log('created user');
     goto('Get Users');
   }, function (error, response, body) {
-    console.log(response.statusCode, body);
   });
 }).registerTask('Get Users', function (goto, site, state) {
   site.get('GET Users', '/users', function (response, body) {
+    console.log('created user');
     goto('Get Users', 100);
   }, function (error, response, body) {
     console.log(error, response);
